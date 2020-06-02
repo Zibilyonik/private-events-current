73a95caee19c43d495f594401fceedce6ab24d87
Zibilyonik <antagonistpotato@gmail.com>
2020-06-02 20:18:29 +0200

    Associations between events and users added

--- app/models/User.rb
+++ app/models/User.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   
     validates :email, presence: true, uniqueness: true
     validates :name, presence: true
-  
+    has_many :events, dependent: :destroy
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_224045) do
+ActiveRecord::Schema.define(version: 2020_06_02_181320) do
 
   create_table "events", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_06_01_224045) do
     t.datetime "date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_06_01_224045) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
