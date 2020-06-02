278b02891c1d15cae34dc308dbd613e2242c2113
Zibilyonik <antagonistpotato@gmail.com>
2020-06-02 20:36:59 +0200

    Events created by the user will be displayed on the Welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :sessions
+  resources :events
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
