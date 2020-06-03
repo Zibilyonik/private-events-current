b1efa29fdf80e2d10e0965ed14470c34016e0368
Zibilyonik <antagonistpotato@gmail.com>
2020-06-04 00:27:06 +0200

    added attendances list to events for attendees and users for attending events, attendances limited to one per person for one event

--- app/models/Attendance.rb
+++ app/models/Attendance.rb
@@ -1,4 +1,5 @@
 class Attendance < ApplicationRecord
     belongs_to :event
     belongs_to :user
+    validates :event_id, uniqueness: { scope: :user_id, message: "You are already attending" }
   end
