f3c6a65864f402ba2a5be8184c52597524c31e09
Zibilyonik <antagonistpotato@gmail.com>
2020-06-04 18:19:48 +0200

    attendance association issues fixed

--- app/models/User.rb
+++ app/models/User.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
     validates :email, presence: true, uniqueness: true
     validates :name, presence: true
     has_many :events, dependent: :destroy
-    has_many :attended_events, through: :attendances, class_name: "event", foreign_key: :event_id
+    has_many :attendances
+    has_many :attended_events, through: :attendances, source: :event, foreign_key: :event_id
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ApplicationRecord
     validates :description, presence: true
     validates :date, presence: true
     belongs_to :creator, class_name: "User", foreign_key: :user_id
-    has_many :attendees, through: :attendances, class_name: "User", foreign_key: :user_id
+    has_many :attendances
+    has_many :attendees, through: :attendances, source: :user, foreign_key: :user_id
 end
