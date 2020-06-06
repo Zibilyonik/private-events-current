7051fb89ed8cd8f6e2598a2b8f65df7426f27dcf
Zibilyonik <antagonistpotato@gmail.com>
2020-06-07 01:49:14 +0200

    user_id in event database table has been replaced with creator_id and all the changes in the model done accordingly

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   validates :title, presence: true
   validates :date, presence: true
-  belongs_to :creator, class_name: 'User', foreign_key: :user_id
+  belongs_to :creator, class_name: 'User'
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
   scope :past, -> { where('date < ?', Time.current) }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_212021) do
+ActiveRecord::Schema.define(version: 2020_06_06_230844) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 2020_06_03_212021) do
     t.datetime "date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +39,5 @@ ActiveRecord::Schema.define(version: 2020_06_03_212021) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "events", "users"
+  add_foreign_key "events", "users", column: "creator_id"
 end
