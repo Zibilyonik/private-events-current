39657f60fc2327c017391ae43b1b66bc77228922
Zibilyonik <antagonistpotato@gmail.com>
2020-06-09 23:35:18 +0200

    style errors fixed

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  it "should not save without title" do
-    event = Event.new(:date => Time.current)
+  it 'should not save without title' do
+    event = Event.new(date: Time.current)
     expect(event).to_not be_valid
   end
 
-  it "should not save without date" do
-    event = Event.new(:title => "Title")
+  it 'should not save without date' do
+    event = Event.new(title: 'Title')
     expect(event).to_not be_valid
   end
 
-  context "with all validations passed" do
+  context 'with all validations passed' do
     before(:each) do
-      @user = User.new(:name => "Potato", :email => "pot@ato.com")
-      @event = Event.new(:title => "Potato", :date => Time.current, :creator => @user)
+      @user = User.new(name: 'Potato', email: 'pot@ato.com')
+      @event = Event.new(title: 'Potato', date: Time.current, creator: @user)
     end
 
-    it "should save with all validations provided" do
+    it 'should save with all validations provided' do
       expect(@event).to be_valid
     end
 
-    it "should return user as Creator" do
+    it 'should return user as Creator' do
       expect(@event.creator).to eq @user
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "should not save without name" do
-    user = User.new(:email => "pot@ato.com")
+  it 'should not save without name' do
+    user = User.new(email: 'pot@ato.com')
     expect(user).to_not be_valid
   end
 
-  it "should not save without email" do
-    user = User.new(:name => "Potato")
+  it 'should not save without email' do
+    user = User.new(name: 'Potato')
     expect(user).to_not be_valid
   end
-  it "should save with all validations provided" do
-    user = User.new(:name => "Potato", :email => "pot@ato.com")
+  it 'should save with all validations provided' do
+    user = User.new(name: 'Potato', email: 'pot@ato.com')
     expect(user).to be_valid
   end
 end
